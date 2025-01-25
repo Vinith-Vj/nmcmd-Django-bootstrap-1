280842ef20f3bef74b9e921c05877e33af50cc31
Vinith-Vj <vinithvj555@gmail.com>
2025-01-25 14:42:06 +0530

    changes

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='services/')  # Images will be stored in /static/services/
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    return render(request, 'index.html', {'services': services})
